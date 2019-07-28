f69cac50faadb28f07029c7c59c4a68485ee50c9
Laurence <laurencemintah@gmail.com>
2019-07-28 15:28:37 -0400

    Updates

---
+++ precache-manifest.81fce42625221f0fb82c1d7154443c53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1c6744b89e6a9e97bae24d437d66273",
+    "url": "/recognize/index.html"
+  },
+  {
+    "revision": "bdad4ab7ce75586f4acb",
+    "url": "/recognize/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "4b20e9a038fffeeb9ddb",
+    "url": "/recognize/static/css/main.61fe2917.chunk.css"
+  },
+  {
+    "revision": "bdad4ab7ce75586f4acb",
+    "url": "/recognize/static/js/2.80cc5aff.chunk.js"
+  },
+  {
+    "revision": "4b20e9a038fffeeb9ddb",
+    "url": "/recognize/static/js/main.af2afbaf.chunk.js"
+  },
+  {
+    "revision": "75996828a85cc3d38b07",
+    "url": "/recognize/static/js/runtime~main.fd73abb3.js"
+  }
+]);
